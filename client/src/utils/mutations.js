b452d3443d8f015ce16f7d1151448ff2b4ac9250
Horace HOU <HoraceHou@CoatesGroup.com>
2023-10-28 09:05:04 +1100

    added queries and mutation to the frontend

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,248 @@
 import { gql } from '@apollo/client';
 
+
+export const CREATE_EMPLOYEE = gql`
+    mutation CreateEmployee($employee: EmployeeInput!) {
+        createEmployee(employee: $employee) {
+            _id
+            employeeID
+            firstname
+            lastname
+            email
+            roleID
+            avatarURI
+        }
+    }
+`;
+
+
+export const UPDATE_EMPLOYEE = gql`
+    mutation UpdateEmployee($employee: EmployeeInput!) {
+        updateEmployee(employee: $employee) {
+            _id
+            employeeID
+            firstname
+            lastname
+            email
+            password
+            roleID
+            avatarURI
+        }
+    }
+`;
+
+
+export const DELETE_EMPLOYEE = gql`
+    mutation DeleteEmployee($employeeId: String!) {
+        deleteEmployee(employeeID: $employeeId)
+    }
+`;
+
+
+export const CREATE_THOUGHT = gql`
+    mutation CreateThought($thought: ThoughtInput!) {
+        createThought(thought: $thought) {
+            _id
+            thoughtID
+            title
+            description
+            comments {
+                _id
+                commentID
+                commentContent
+                postedTime
+                EmployeeID
+            }
+            datePosted
+            EmployeeID
+        }
+    }
+`;
+
+
+export const DELETE_THOUGHT = gql`
+    mutation DeleteThought($thoughtId: String!) {
+        deleteThought(thoughtID: $thoughtId)
+    }
+`;
+
+
+export const CREATE_COMMENT = gql`
+    mutation CreateComment($thoughtId: String!, $comment: CommentInput!) {
+        createComment(thoughtID: $thoughtId, comment: $comment) {
+            _id
+            thoughtID
+            title
+            description
+            comments {
+                _id
+                commentID
+                commentContent
+                postedTime
+                EmployeeID
+            }
+            datePosted
+            EmployeeID
+        }
+    }
+`;
+
+export const DELELTE_COMMENT = gql`
+    mutation DeleteComment($thoughtId: String!, $commentId: String!) {
+        deleteComment(thoughtID: $thoughtId, commentID: $commentId) {
+            _id
+            thoughtID
+            title
+            description
+            comments {
+                _id
+                commentID
+                commentContent
+                postedTime
+                EmployeeID
+            }
+            datePosted
+            EmployeeID
+        }
+    }
+`;
+
+export const CREATE_TASK = gql`
+    mutation CreateTask($task: TaskInput!) {
+        createTask(task: $task) {
+            _id
+            taskID
+            taskName
+            taskDescription
+            todos {
+                _id
+                todoID
+                name
+                description
+                completed
+                completionTime
+                EmployeeIDs
+            }
+            startingTime
+            targetTime
+            completionTime
+            completed
+            EmployeeIDs
+        }
+    }
+`;
+
+export const UPDATE_TASK = gql`
+    mutation UpdateTask($task: TaskInput!) {
+        updateTask(task: $task) {
+            _id
+            taskID
+            taskName
+            taskDescription
+            todos {
+                _id
+                todoID
+                name
+                description
+                completed
+                completionTime
+                EmployeeIDs
+            }
+            startingTime
+            targetTime
+            completionTime
+            completed
+            EmployeeIDs
+        }
+    }
+`;
+
+export const DELETE_TASK = gql`
+    mutation Mutation($taskId: String!) {
+    deleteTask(taskID: $taskId)
+    }
+`;
+
+export const CREATE_TODO = gql`
+    mutation CreateToDo($taskId: String!, $todo: ToDoInput!) {
+        createToDo(taskID: $taskId, todo: $todo) {
+            _id
+            taskID
+            taskName
+            taskDescription
+            todos {
+                _id
+                todoID
+                name
+                description
+                completed
+                completionTime
+                EmployeeIDs
+            }
+            startingTime
+            targetTime
+            completionTime
+            completed
+            EmployeeIDs
+        }
+    }
+`;
+
+export const UPDATE_TODO = gql`
+    mutation UpdateToDo($taskId: String!, $todo: ToDoInput!) {
+        updateToDo(taskID: $taskId, todo: $todo) {
+            _id
+            taskID
+            taskName
+            taskDescription
+            todos {
+                _id
+                todoID
+                name
+                description
+                completed
+                completionTime
+                EmployeeIDs
+            }
+            startingTime
+            targetTime
+            completionTime
+            completed
+            EmployeeIDs
+        }
+    }
+`;
+
+export const DELETE_TODO = gql`
+    mutation DeleteToDo($taskId: String!, $todoId: String!) {
+        deleteToDo(taskID: $taskId, todoID: $todoId) {
+            _id
+            taskID
+            taskName
+            taskDescription
+            todos {
+                _id
+                todoID
+                name
+                description
+                completed
+                completionTime
+                EmployeeIDs
+            }
+            startingTime
+            targetTime
+            completionTime
+            completed
+            EmployeeIDs
+        }
+    }
+`;
+
+
+
+
+
+
 export const LOGIN_USER = gql`
     mutation Login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
@@ -29,31 +272,6 @@ export const SAVE_BOOK = gql`
                 link
                 title
             }
-        }
-    }
-`;
-
-export const CREATE_TODO = gql`
-    mutation CreateToDo($taskId: String!, $todo: ToDoInput!) {
-        createToDo(taskID: $taskId, todo: $todo) {
-            _id
-            taskID
-            taskName
-            taskDescription
-            todos {
-                _id
-                todoID
-                name
-                description
-                completed
-                completionTime
-                EmployeeIDs
-            }
-            startingTime
-            targetTime
-            completionTime
-            completed
-            EmployeeIDs
         }
     }
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,25 +1,5 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USER = gql`
-    query User($username: String!) {
-        user(username: $username) {
-            _id
-            username
-            email
-            password
-            savedBooks {
-                _id
-                authors
-                description
-                bookId
-                image
-                link
-                title
-            }
-        }
-    }
-`;
-
 export const QUERY_TASKS = gql`
     query Tasks {
         tasks {
@@ -45,6 +25,36 @@ export const QUERY_TASKS = gql`
     }
 `;
 
+export const QUERY_EMPLOYEE = gql`
+    query Employees($employeeId: String!) {
+        employee(employeeID: $employeeId) {
+            _id
+            employeeID
+            firstname
+            lastname
+            email
+            password
+            roleID
+            avatarURI
+        }
+    }
+`;
+
+export const QUERY_EMPLOYEES = gql`
+    query Roles {
+        employees {
+            _id
+            employeeID
+            firstname
+            lastname
+            email
+            password
+            roleID
+            avatarURI
+        }
+    }
+`;
+
 export const QUERY_EMPLOYEES_PROFILE_INFO = gql`
     query Employees {
         employees {
@@ -52,6 +62,82 @@ export const QUERY_EMPLOYEES_PROFILE_INFO = gql`
             avatarURI
             firstname
             lastname
+        }
+    }
+`;
+
+
+export const QUERY_ROLES = gql`
+    query Roles {
+        roles {
+            _id
+            roleID
+            roleName
+        }
+    }
+`;
+
+export const QUERY_THOUGHTS = gql`
+    query Thoughts {
+        thoughts {
+            _id
+            thoughtID
+            title
+            description
+            comments {
+                _id
+                commentID
+                commentContent
+                postedTime
+                EmployeeID
+            }
+            datePosted
+            EmployeeID
+        }
+    }
+`;
+
+export const QUERY_TASK = gql`
+    query Task($taskId: String!) {
+        task(taskID: $taskId) {
+            _id
+            taskID
+            taskName
+            taskDescription
+            todos {
+                    _id
+                    todoID
+                    name
+                    description
+                    completed
+                    completionTime
+                    EmployeeIDs
+                }
+            startingTime
+            targetTime
+            completionTime
+            completed
+            EmployeeIDs
+        }
+    }
+`;
+
+export const QUERY_USER = gql`
+    query User($username: String!) {
+        user(username: $username) {
+            _id
+            username
+            email
+            password
+            savedBooks {
+                _id
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
         }
     }
 `;
