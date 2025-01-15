88d31d6e59aa3ff6ad2cd6f022457318c22bafb1
Ruihang2017 <94610820+Ruihang2017@users.noreply.github.com>
2025-01-15 22:23:40 +1100

    Updated ToDoTableRow.jsx

--- client/src/components/table/ToDoTableRow.jsx
+++ client/src/components/table/ToDoTableRow.jsx
@@ -1,50 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import {
     Avatar,
-    Badge,
-    Box,
     Checkbox,
     HStack,
     Icon,
     IconButton,
-    Table,
-    Tbody,
     Td,
     Text,
-    Th,
-    Thead,
     Tr,
-    useDisclosure,
     Input,
     useColorModeValue,
 } from '@chakra-ui/react'
-import { FiEdit2, FiTrash2, FiCheckSquare, FiSave, FiDelete } from 'react-icons/fi'
-import { IoEllipsisHorizontal } from "react-icons/io5";
+import { FiEdit2, FiSave, FiDelete } from 'react-icons/fi'
 import { TiUserAddOutline } from "react-icons/ti";
-
-
-import { useState, useEffect } from 'react';
-import { TaskModal } from '../modal/TaskModal';
 import TodoMenu from "@components/menu/TodoMenu";
 
-
-
-const ToDoTableRow = (props) => {
-    // { todo, taskEmployees }
-    // const [todoCompleted, setTodoCompleted] = useState(false);
-    const [todo, setTodo] = useState(props.todo);
-    const [taskEmployees, setTaskEmployees] = useState(props.taskEmployees);
-
+/**
+ * ToDoTableRow component that represents a row in the to-do table.
+ * @param {Object} props The properties of the component.
+ * @param {Object} props.todo The to-do object.
+ * @param {Array} props.taskEmployees The task employees object.
+ * @param {Function} props.updateToDo The function to update a to-do.
+ * @param {Function} props.deleteToDo The function to delete a to-do.
+ * @param {Function} props.changeTaskCompleted The function to change the task completed status.
+ * @returns {JSX.Element} The JSX element representing the component.
+ */
+const ToDoTableRow = ({
+    todo: initialTodo,
+    taskEmployees: initialTaskEmployees,
+    updateToDo,
+    deleteToDo,
+    changeTaskCompleted
+    }) => {
+    const [todo, setTodo] = useState(initialTodo);
+    const [taskEmployees, setTaskEmployees] = useState(initialTaskEmployees);
     const [availableTaskEmployees, setAvailableTaskEmployees] = useState(() => {
-        return taskEmployees.filter(taskEmployee => {
-            if (!todo.EmployeeIDs.find(id => id === taskEmployee.employeeID)) {
-                return taskEmployee
-            }
-        });
+        return initialTaskEmployees.filter(taskEmployee => !initialTodo.EmployeeIDs.includes(taskEmployee.employeeID));
     });
+    const [isEdit, setIsEdit] = useState(false);
+    const textColor = useColorModeValue("gray.700", "white");
 
     useEffect(() => {
-        setTaskEmployees(props.taskEmployees);
-    }, [props.taskEmployees]);
+        setTaskEmployees(initialTaskEmployees);
+    }, [initialTaskEmployees]);
 
     useEffect(() => {
         setAvailableTaskEmployees(() => {
@@ -54,145 +53,158 @@ const ToDoTableRow = (props) => {
                 }
             });
         })
-    }, [taskEmployees]);
+    }, [taskEmployees, todo.EmployeeIDs]);
 
-
-    const [isEdit, setIsEdit] = useState(false);
-
-    const textColor = useColorModeValue("gray.700", "white");
-
-
-    const addToDoEmployee = (data) => {
-        setTodo({
-            ...todo,
-            EmployeeIDs: [...todo.EmployeeIDs, data.employeeID],
-        });
-
-        const newTaskEmployees = availableTaskEmployees.filter(taskEmployee => taskEmployee.employeeID !== data.employeeID);
-        setAvailableTaskEmployees(newTaskEmployees)
+    const addToDoEmployee = (employee) => {
+        setTodo(prevTodo => ({
+            ...prevTodo,
+            EmployeeIDs: prevTodo.EmployeeIDs.filter(id => id !== employee.employeeID),
+        }));
+        setAvailableTaskEmployees(prevEmployees => [...prevEmployees, employee]);
     }
 
-    const removeToDoEmployee = (data) => {
-        const newTaskEmployees = todo.EmployeeIDs.filter(EmployeeIDs => EmployeeIDs !== data.employeeID);
-        setTodo({
-            ...todo,
-            EmployeeIDs: [...newTaskEmployees],
-        });
-        setAvailableTaskEmployees([...availableTaskEmployees, data])
-    }
+    const removeToDoEmployee = (employee) => {
+        setTodo(prevTodo => ({
+            ...prevTodo,
+            EmployeeIDs: prevTodo.EmployeeIDs.filter(id => id !== employee.employeeID),
+        }));
+        setAvailableTaskEmployees(prevEmployees => [...prevEmployees, employee]);
+    };
 
     return (
-        <>
-            <Tr key={todo.todoID}>
-                <Td>
-                    <Checkbox
-                        isChecked={todo.completed}
-                        onChange={(event) => {
-                            if (isEdit) return;
-                            const changedTodo = {
-                                ...todo,
-                                completed: event.target.checked,
-                            }
-                            setTodo(changedTodo)
-                            props.updateToDo(changedTodo);
-                            if (!event.target.checked) {
-                                props.changeTaskCompleted(false);
-                            }
-
-                        }} />
-                </Td>
-                <Td>
-                    {isEdit ?
-                        (<Input px="0" maxW={{ md: '3xl', }} defaultValue={todo.name}
-                            onChange={(event) => {
-                                setTodo({
-                                    ...todo,
-                                    name: event.target.value,
-                                })
-                            }}
-                        />)
-                        :
-                        (<Text color="fg.muted">{todo.name}</Text>)}
-                </Td>
-                <Td>
-                    {isEdit ?
-                        (<Input px="0" maxW={{ md: '3xl', }} defaultValue={todo.description}
-                            onChange={(event) => {
-                                setTodo({
-                                    ...todo,
-                                    description: event.target.value,
-                                })
-                            }}
-                        />)
-                        :
-                        (<Text color="fg.muted">{todo.description}</Text>)}
-                </Td>
-                <Td>
-
-                    <HStack spacing="2">
+        <Tr key={todo.todoID}>
+            <Td>
+                <Checkbox
+                    isChecked={todo.completed}
+                    onChange={(event) => {
+                        if (isEdit) return;
+                        const updatedTodo = {
+                            ...todo,
+                            completed: event.target.checked,
+                        };
+                        setTodo(updatedTodo);
+                        updateToDo(updatedTodo);
+                        if (!event.target.checked) {
+                            changeTaskCompleted(false);
+                        }
+                    }}
+                />
+            </Td>
+            <Td>
+                {isEdit ? (
+                    <Input
+                        px="0"
+                        maxW={{ md: '3xl' }}
+                        defaultValue={todo.name}
+                        onChange={(event) => setTodo({ ...todo, name: event.target.value })}
+                    />
+                ) : (
+                    <Text color="fg.muted">{todo.name}</Text>
+                )}
+            </Td>
+            <Td>
+                {isEdit ? (
+                    <Input
+                        px="0"
+                        maxW={{ md: '3xl' }}
+                        defaultValue={todo.description}
+                        onChange={(event) => setTodo({ ...todo, description: event.target.value })}
+                    />
+                ) : (
+                    <Text color="fg.muted">{todo.description}</Text>
+                )}
+            </Td>
+            <Td>
+                <HStack spacing="2">
+                    {isEdit ? (
                         <>
-                            {isEdit ?
-                                (<>
-                                    <TodoMenu
-                                        taskEmployees={availableTaskEmployees}
-                                        addToDoEmployee={addToDoEmployee}
-                                        icon={<Icon as={TiUserAddOutline} w='24px' h='24px' color={textColor} />} />
-                                    {taskEmployees.map(taskEmployee => {
-                                        if (todo.EmployeeIDs.find(EmployeeID => EmployeeID === taskEmployee.employeeID)) {
-                                            return <Avatar
-                                                key={taskEmployee.employeeID}
-                                                onClick={() => removeToDoEmployee(taskEmployee)}
-                                                size="sm" name="Christoph Winston" src={taskEmployee.avatarURI} />
-                                        }
-                                    })
-                                    }
-                                </>
-                                ) : (<>
-                                    {taskEmployees.map(taskEmployee => {
-                                        if (todo.EmployeeIDs.find(EmployeeID => EmployeeID === taskEmployee.employeeID)) {
-                                            return <Avatar
-                                                key={taskEmployee.employeeID}
-                                                // onClick={() => removeToDoEmployee(taskEmployee)}
-                                                size="sm" name="Christoph Winston" src={taskEmployee.avatarURI} />
-                                        }
-                                    })
-                                    }</>)}
+                            <TodoMenu
+                                taskEmployees={availableTaskEmployees}
+                                addToDoEmployee={addToDoEmployee}
+                                icon={<Icon as={TiUserAddOutline} w="24px" h="24px" color={textColor} />}
+                            />
+                            {taskEmployees.map(taskEmployee => (
+                                todo.EmployeeIDs.includes(taskEmployee.employeeID) && (
+                                    <Avatar
+                                        key={taskEmployee.employeeID}
+                                        onClick={() => removeToDoEmployee(taskEmployee)}
+                                        size="sm"
+                                        name={taskEmployee.name}
+                                        src={taskEmployee.avatarURI}
+                                    />
+                                )
+                            ))}
                         </>
-                    </HStack>
-                </Td>
-                <Td>
-                    <HStack spacing="1">
-                        {isEdit ?
-                            (
-                                <>
-                                    <IconButton icon={<FiSave />} variant="tertiary" aria-label="Edit task"
-                                        onClick={() => {
-                                            setIsEdit(false);
-                                            props.updateToDo(todo);
-                                        }} />
-                                    <IconButton icon={<FiDelete />} variant="tertiary" aria-label="Edit task"
-                                        onClick={() => {
-                                            setIsEdit(true);
-                                            props.deleteToDo(todo);
-                                        }} />
-                                </>
+                    ) : (
+                        taskEmployees.map(taskEmployee => (
+                            todo.EmployeeIDs.includes(taskEmployee.employeeID) && (
+                                <Avatar
+                                    key={taskEmployee.employeeID}
+                                    size="sm"
+                                    name={taskEmployee.name}
+                                    src={taskEmployee.avatarURI}
+                                />
                             )
-                            :
-                            (
-                                <>
-                                    {todo.completed ?
-                                        (<></>) :
-                                        (<IconButton icon={<FiEdit2 />} variant="tertiary" aria-label="Edit task"
-                                            onClick={() => {
-                                                setIsEdit(true);
-                                            }} />)}
-                                </>
-                            )}
-                    </HStack>
-                </Td>
-            </Tr >
-        </>
-    )
+                        ))
+                    )}
+                </HStack>
+            </Td>
+            <Td>
+                <HStack spacing="1">
+                    {isEdit ? (
+                        <>
+                            <IconButton
+                                icon={<FiSave />}
+                                variant="tertiary"
+                                aria-label="Save task"
+                                onClick={() => {
+                                    setIsEdit(false);
+                                    updateToDo(todo);
+                                }}
+                            />
+                            <IconButton
+                                icon={<FiDelete />}
+                                variant="tertiary"
+                                aria-label="Delete task"
+                                onClick={() => {
+                                    setIsEdit(false);
+                                    deleteToDo(todo);
+                                }}
+                            />
+                        </>
+                    ) : (
+                        !todo.completed && (
+                            <IconButton
+                                icon={<FiEdit2 />}
+                                variant="tertiary"
+                                aria-label="Edit task"
+                                onClick={() => setIsEdit(true)}
+                            />
+                        )
+                    )}
+                </HStack>
+            </Td>
+        </Tr>
+    );
 }
+
+// Define prop types for the component
+ToDoTableRow.propTypes = {
+    todo: PropTypes.shape({
+        todoID: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        completed: PropTypes.bool.isRequired,
+        EmployeeIDs: PropTypes.arrayOf(PropTypes.string).isRequired,
+    }).isRequired,
+    taskEmployees: PropTypes.arrayOf(PropTypes.shape({
+        employeeID: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        avatarURI: PropTypes.string,
+    })).isRequired,
+    updateToDo: PropTypes.func.isRequired,
+    deleteToDo: PropTypes.func.isRequired,
+    changeTaskCompleted: PropTypes.func.isRequired,
+};
 
 export default ToDoTableRow;
