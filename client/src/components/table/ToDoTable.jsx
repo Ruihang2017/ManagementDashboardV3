e1cf875cdf4de6b60e1f24880ea5ddc610ba44dd
Ruihang2017 <94610820+Ruihang2017@users.noreply.github.com>
2025-01-29 19:05:38 +1100

    updated .env

--- client/src/components/table/ToDoTable.jsx
+++ client/src/components/table/ToDoTable.jsx
@@ -1,37 +1,23 @@
-import {
-    Avatar,
-    Badge,
-    Box,
-    Checkbox,
-    HStack,
-    Icon,
-    IconButton,
-    Table,
-    Tbody,
-    Td,
-    Text,
-    Th,
-    Thead,
-    Tr,
-    useDisclosure,
-    Input,
-    useColorModeValue,
-} from '@chakra-ui/react'
-import { FiEdit2, FiTrash2, FiCheckSquare } from 'react-icons/fi'
-import { IoEllipsisHorizontal } from "react-icons/io5";
-import { TiUserAddOutline } from "react-icons/ti";
-import { useQuery, useMutation } from '@apollo/client';
 import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import { useMutation } from '@apollo/client';
 import { v4 as uuidv4 } from 'uuid';
 
-
-import { TaskModal } from '../modal/TaskModal';
-import TodoMenu from "@components/menu/TodoMenu";
 import ToDoTableRow from "@components/table/ToDoTableRow";
 import NewToDo from "@components/table/NewToDo";
 import { CREATE_TODO, UPDATE_TODO, DELETE_TODO } from '@utils/mutations';
 
-
+/**
+ * TodoTable component that displays a table of to-dos.
+ * @param {Object} props - The component props.
+ * @param {Array} props.todos - The list of to-dos.
+ * @param {Array} props.taskEmployees - The list of task employees.
+ * @param {String} props.taskID - The task ID.
+ * @param {Function} props.setTask - The function to set the task.
+ * @param {Function} props.changeTaskCompleted - The function to change the task completed status.
+ * @returns {JSX.Element} The JSX element representing the component.
+ */
 export const TodoTable = (props) => {
     // { todos, setTask, taskEmployees, taskID }
     const [todos, setTodos] = useState(props.todos)
--- client/src/pages/Task.jsx
+++ client/src/pages/Task.jsx
@@ -1,47 +1,40 @@
+import React from 'react';
 import {
     Box,
     Container,
     HStack,
     Stack,
     Text,
-    useBreakpointValue,
     useColorModeValue,
-} from '@chakra-ui/react'
-import { TaskTable } from '../components/table/TaskTable'
+} from '@chakra-ui/react';
+import { TaskTable } from '../components/table/TaskTable';
 import { useQuery } from '@apollo/client';
 import { QUERY_TASKS, QUERY_EMPLOYEES_PROFILE_INFO } from '../utils/queries';
-
 
 /**
  * Task component that displays a table of tasks and employee profile information.
  * @returns {JSX.Element} The rendered component.
  */
-export default function Task() {
+const Task = () => {
     const paleGray = useColorModeValue("secondaryGray.400", "whiteAlpha.100");
     const white = useColorModeValue('white', 'navy.900');
 
     // Query for fetching tasks
-    const { loading: loadingQueryTask, data: dataQueryTask } = useQuery(QUERY_TASKS);
-    const taskData = dataQueryTask?.tasks || [];
+    const { loading: loadingTasks, data: taskData } = useQuery(QUERY_TASKS);
+    const tasks = taskData?.tasks || [];
 
     // Query for fetching employees profile info
-    const { loading: loadingQueryEmployeesProfileInfo, data: dataQueryEmployeesProfileInfo } = useQuery(QUERY_EMPLOYEES_PROFILE_INFO);
-    const employeesProfileInfo = dataQueryEmployeesProfileInfo?.employees || [];
+    const { loading: loadingEmployees, data: employeeData } = useQuery(QUERY_EMPLOYEES_PROFILE_INFO);
+    const employeesProfileInfo = employeeData?.employees || [];
 
-
-    useBreakpointValue({
-        base: true,
-        md: false,
-    })
-
-    if (loadingQueryTask || loadingQueryEmployeesProfileInfo) {
+    // Handle loading state
+    if (loadingTasks || loadingEmployees) {
         return <div>Loading...</div>;
     }
 
-
     return (
-        <Box height='100%' bgColor={paleGray}>
-            <Container maxW='100%' py={{ base: '4', md: '8' }} px={{ base: '0', md: '8' }}>
+        <Box height="100%" bgColor={paleGray}>
+            <Container maxW="100%" py={{ base: '4', md: '8' }} px={{ base: '0', md: '8' }}>
                 <Box
                     bgColor={white}
                     boxShadow={{ base: 'none', md: 'sm' }}
@@ -56,11 +49,13 @@ export default function Task() {
                             </HStack>
                         </Box>
                         <Box overflowX="auto">
-                            <TaskTable taskData={taskData} employeesProfileInfo={employeesProfileInfo} />
+                            <TaskTable taskData={tasks} employeesProfileInfo={employeesProfileInfo} />
                         </Box>
                     </Stack>
                 </Box>
             </Container>
         </Box>
     );
-}
+};
+
+export default Task;
